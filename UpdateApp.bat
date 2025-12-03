001552189d358059820aae82eca7932f038a5f93
Cvek Jan <jan.cvek@akrapovic.com>
2025-12-03 16:35:31 +0100

    update updateApp batch

--- UpdateApp.bat
+++ UpdateApp.bat
@@ -3,8 +3,12 @@ REM --- Fetch in pull najnovejše spremembe ---
 git fetch
 git pull
 
+echo.
+echo SKRIPTA JE POSODOBLJENA NA NAJNOVEJSO VERZIJO.
+echo.
+echo IZVEDLO SE BO NADGRADNJE PAKETOV....
+echo.
+
 cmd /k "cd /d %~dp0\.venv\Scripts & call activate & cd /d %~dp0 & python -m pip install --upgrade pip & python -m pip install -r requirements.txt
 
-echo.
-echo Skripta je posodobljena
 pause
